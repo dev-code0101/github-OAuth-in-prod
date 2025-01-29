e4e9f38e5dbe2b7fa0df3e20f9e0cc0c43e5c192
dev-code0101 <dev.codeone@proton.me>
2025-01-29 20:58:31 +0530

    DEV: reslve missing imports

--- app/db.py
+++ app/db.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-from app.config import DATABASE_URL
+from app.config import settings
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
 )
