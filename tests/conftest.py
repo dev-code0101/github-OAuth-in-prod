23cc7a2222bf4797eeff86de23322325f9d543f5
dev-code0101 <dev.codeone@proton.me>
2025-01-29 22:03:10 +0530

    FIX: same issue in other files

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.main import app
-from app.database import get_session, Base, engine
+from app.db import get_session, Base, engine
 
 
 # Override the database session to use an in-memory test database
